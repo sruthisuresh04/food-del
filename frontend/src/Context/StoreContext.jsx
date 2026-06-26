5ed81068c02e52ba52ed9abae93fb53a9c0bc66c
GitHub <sruthisru2002@gmail.com>
2026-06-26 19:58:05 +0530

    Update frontend backend URL

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url  = import.meta.env.VITE_BACKEND_URL;
 
   const [token,setToken] =  useState("");
   const [food_list,setFoodList] = useState([]);
