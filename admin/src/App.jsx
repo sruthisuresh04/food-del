d894b362280e127b06375a06068a7715c7d411a5
sruthisuresh <sruthisru2002@gmail.com>
2026-06-26 20:09:31 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import { Navigate } from 'react-router-dom';
 // ...existing imports...
 
 const App = () => {
-const url = 'http://localhost:4000'
+const url = import.meta.env.VITE_BACKEND_URL;
 
   return (
     <div>
